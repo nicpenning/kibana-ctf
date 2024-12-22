7deb8bfe3ca76fb12495e8e8bcfdd16294ed5ae9
Nic <nicpenning@gmail.com>
2024-12-21 20:00:22 -0600

    Create Invoke-Kibana-CTF-Setup.ps1

---
+++ Invoke-Kibana-CTF-Setup.ps1
@@ -0,0 +1,16 @@
+# Setup
+
+# Options
+# 1. Deploy CTFd
+# 2. Deploy Elastic Stack
+# 3. Import CTFd Challenges (All or Specfic Challenges)
+# 4. Import Objects and Index Documents for Elastic Stack
+# 5. Reset CTFd
+# 6. Reset Elastic Stack
+# 7. Check for Requirements 
+
+# Check for running Elastic Stack
+
+# Check for running CTFd
+
+# 
