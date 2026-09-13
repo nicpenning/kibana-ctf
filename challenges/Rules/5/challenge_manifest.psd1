a696578516ba92b921dffb24e743ee58444b4c22
nicpenning <nicpenning@gmail.com>
2026-09-12 21:46:51 -0500

    Add 5th challenge, adjust roles accordingly

---
+++ challenges/Rules/5/challenge_manifest.psd1
@@ -0,0 +1,11 @@
+@{
+    Name = "A Call to Action"
+    Category = "Rules"
+    RequiredFiles = @(
+        "ctfd_challenge.json"
+        "ctfd_flag.json"
+    )
+    Resources = @{
+        KibanaVersion = "^9.5.3"
+    }
+}
---
+++ challenges/Rules/5/elastic_import_script.ps1
@@ -0,0 +1,12 @@
+# Advanced Elastic Stack Import Script for Challenge: High Quality Rules Only - No Exceptions!
+function challenge {
+    # Create an exception for the calc.exe detection rule in Elastic Stack using the API
+    # Requires overwrite_exceptions=true parameter to be set in the import API call
+
+    # Import the calc.exe detection rule with the exception
+
+    $ruleFilePath = "./challenges/Rules/5/elastic_rule_with_exceptions_and_actions.ndjson"
+    $importRuleResponse = Invoke-RestMethod -Method POST -Uri "$Kibana_URL/s/kibana-ctf/api/detection_engine/rules/_import?overwrite=true&overwrite_exceptions=true" -Headers @{"kbn-xsrf"="true"; "Authorization"="$kibanaAuth"} -Form @{file = Get-Item $ruleFilePath} -AllowUnencryptedAuthentication -SkipCertificateCheck
+
+    return Write-Debug "✅ elastic_import_script.ps1 executed"
+}
