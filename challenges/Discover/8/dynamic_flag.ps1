592a2575f0661cd9c4de15beaf229aac1c399b79
nicpenning <nicpenning@gmail.com>
2026-09-14 12:53:13 -0500

    Refactor Discover/8 Time picker to make work and make dynamic date as well

--- challenges/Discover/8/dynamic_flag.ps1
+++ challenges/Discover/8/dynamic_flag.ps1
@@ -1,14 +1,22 @@
 function dynamic_flag {
     $flag_file_path = './challenges/Discover/8/ctfd_flag.json'
+    $saved_object_file_path = './challenges/Discover/8/elastic_saved_objects.ndjson'
     $ctfd_flag = Get-Content $flag_file_path | ConvertFrom-Json -Depth 10
+    $saved_object = Get-Content $saved_object_file_path | ConvertFrom-Json -Depth 10
 
     # Adjust dynamic incident challenge
-    if ($ctfd_flag.content -match '38'){
+    if ($ctfd_flag.content -match 'dynamic_date'){
         Write-Debug "Incident Challenge detected, updating dynamic challenge answer."
-        $days = $($($(Get-date)-$(Get-Date 2024-11-12T07:43:13.373Z)).Days)
-        $ctfd_flag.content = $ctfd_flag.content -replace '38', "$($days-1)|$days|$($days+1)"
+        # Calculate a random date between now and 90 days ago.
+        $random_date = (Get-Date).AddDays(-(Get-Random -Minimum 0 -Maximum 91))
+        $isoDate = $random_date.ToString("yyyy-MM-ddTHH:mm:ss.fffZ")
+        $days = ((Get-Date) - $random_date).Days
+        $ctfd_flag.content = $ctfd_flag.content -replace 'dynamic_days', "$($days-1)|$days|$($days+1)"
+        $saved_object.attributes.'timepicker:quickRanges' = $saved_object.attributes.'timepicker:quickRanges' -replace 'dynamic_date', "$isoDate"
     }
     # Create new flag file with new dynamic date
     $ctfd_flag | ConvertTo-Json -Depth 10 | Out-File -FilePath $flag_file_path
+    $saved_object | ConvertTo-Json -Depth 10 -Compress | Out-File -FilePath $saved_object_file_path
+
     return Write-Debug "✅ elastic_import_script.ps1 executed"
 }
